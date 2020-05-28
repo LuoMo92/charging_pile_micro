ee82013a6ce580a0ab9ac022183feea873af3fef
luomo <810754420@qq.com>
2020-05-28 11:16:19 +0800

    练习代码

---
+++ components/Header/myHeader.js
@@ -0,0 +1,26 @@
+// components/Header/myHeader.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    cData:{
+      value:"MyHeader！！！",
+      type:"string"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
